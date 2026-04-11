4bbc163a3ed8be39f32b6209a7f42374ae46694b
William Kim <will.kim0526@gmail.com>
2026-04-11 13:16:10 -0500

    mutex

--- kernel/services/mutex.rs
+++ kernel/services/mutex.rs
@@ -1,1 +1,19 @@
-//use crate::kernel::{KernelState, Priority, TCB_Handle};
+use core::ptr::{null, null_mut};
+
+use crate::kernel::{KernelState, Priority, TCB_Handle};
+
+pub struct Mutex {
+    lock: u32,
+    owner: *mut TCB_Handle,
+    waitlist: *mut TCB_Handle,
+}
+
+impl Mutex {
+    pub fn new() -> Self {
+        Self {
+            lock: 0,
+            owner: null_mut(),
+            waitlist: null_mut(),
+        }
+    }
+}
